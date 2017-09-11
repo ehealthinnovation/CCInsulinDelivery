d9595546812be5ff15c0427694740698f605a97f
Kevin Tallevi <ktallevi@ehealthinnovation.org>
2017-09-11 17:38:35 -0400

    pod template created

---
+++ Example/Pods/Target Support Files/CCInsulinDelivery/CCInsulinDelivery-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CCInsulinDelivery : NSObject
+@end
+@implementation PodsDummy_CCInsulinDelivery
+@end
---
+++ Example/Pods/Target Support Files/Pods-CCInsulinDelivery_Tests/Pods-CCInsulinDelivery_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CCInsulinDelivery_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CCInsulinDelivery_Tests
+@end
